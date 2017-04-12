889cf7b50c94feb64f64b59005929551510f42ed
YYX999 <13890918643@163.com>
2017-04-12 09:59:44 +0800

    QusetionInfo 冲突

--- stmanagermapper/src/main/java/com/st/mapper/QuestionInfoMapper.java
+++ stmanagermapper/src/main/java/com/st/mapper/QuestionInfoMapper.java
@@ -1,36 +1,12 @@
 package com.st.mapper;
 
 import com.st.pojo.QuestionInfo;
-import com.st.pojo.QuestionInfoExample;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface QuestionInfoMapper {
 
-    int countByExample(QuestionInfoExample example);
-
-    int deleteByExample(QuestionInfoExample example);
-
-    int deleteByPrimaryKey(String questionId);
-
-    int insert(QuestionInfo record);
-
-    int insertSelective(QuestionInfo record);
-
-    List<QuestionInfo> selectByExample(QuestionInfoExample example);
-
-    QuestionInfo selectByPrimaryKey(String questionId);
-
-    QuestionInfo selectByQuestionId(String questionId);
-
-    int updateByExampleSelective(@Param("record") QuestionInfo record, @Param("example") QuestionInfoExample example);
-
-    int updateByExample(@Param("record") QuestionInfo record, @Param("example") QuestionInfoExample example);
-
-    int updateByPrimaryKeySelective(QuestionInfo record);
-
-    int updateByPrimaryKey(QuestionInfo record);
 
     /**
      * 根据题目id查询所属试卷信息，卷名
