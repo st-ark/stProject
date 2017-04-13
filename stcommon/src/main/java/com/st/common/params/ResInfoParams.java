b86a234821df6c54439f8fa734998a5af0a1344b
pengzijun <zj.peng1995@gmail.com>
2017-04-13 16:55:58 +0800

    工具类实体类的增加

---
+++ stcommon/src/main/java/com/st/common/params/ResInfoParams.java
@@ -0,0 +1,57 @@
+package com.st.common.params;
+
+/**
+ * Created by Administrator on 2017/4/13.
+ */
+public class ResInfoParams {
+    private String resId;
+    private String title;
+    private int type;
+    private Long knowledgeId;
+    private String upId;
+
+    public Long getKnowledgeId() {
+        return knowledgeId;
+    }
+
+    public void setKnowledgeId(Long knowledgeId) {
+        this.knowledgeId = knowledgeId;
+    }
+
+    public String getResId() {
+        return resId;
+    }
+
+    public void setResId(String resId) {
+        this.resId = resId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+
+
+
+
+    public String getUpId() {
+        return upId;
+    }
+
+    public void setUpId(String upId) {
+        this.upId = upId;
+    }
+}
